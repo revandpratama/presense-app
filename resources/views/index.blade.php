0767e9de52c7aa2e96b2e2d3c03503094e79a7c0
Revand <revandaustin@gmail.com>
2023-03-15 21:49:48 +0700

    none

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,9 @@
 @extends('partials.main')
 
 @section('container')
-    
+    <div class="container">
+        <div class="col-md-8">
+            <p>Blue balls</p>
+        </div>
+    </div>
 @endsection
